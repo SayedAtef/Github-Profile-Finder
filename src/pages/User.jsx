4c3a34ad11626e191cc26fd9f37e22aad890fac7
Sayed Atef <sayedtheone1@gmail.com>
2022-06-10 06:46:35 +0200

    fix env file

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -4,7 +4,7 @@ import { Link, useParams } from "react-router-dom";
 import Spinner from "../components/layout/Spinner";
 import RepoList from "../components/repos/RepoList";
 import GithubContext from "../context/github/GithubContext";
-import { getUserAndRepos } from "../context/github/GithupActions";
+import { getUser, getUserRepos } from "../context/github/GithupActions";
 
 const User = () => {
   const { user, repos, loading, dispatch } = useContext(GithubContext);
@@ -12,8 +12,11 @@ const User = () => {
   useEffect(() => {
     dispatch({ type: "SET_LOADING" });
     const getUserData = async () => {
-      const userData = await getUserAndRepos(params.login);
-      dispatch({ type: "GET_USER_AND_REPOS", payload: userData });
+      const userData = await getUser(params.login);
+      dispatch({ type: "GET_USER", payload: userData });
+
+      const userRepoData = await getUserRepos(params.login);
+      dispatch({ type: "GET_REPOS", payload: userRepoData });
     };
     getUserData();
   }, [dispatch, params.login]);
@@ -42,21 +45,23 @@ const User = () => {
       <div className="w-full mx-auto lg:w-10/12">
         <div className="mb-4">
           <Link to="/" className="btn btn-ghost">
-            Back to Search
+            Back To Search
           </Link>
         </div>
-        <div className="grid grid-cols-1 xl:grid-cols-3  lg:grid-cols-3  md:grid-cols-3 mb-8 md:gap-8">
-          <div className="custom-card-image mb-6 md:mb-0 ">
+
+        <div className="grid grid-cols-1 xl:grid-cols-3 lg:grid-cols-3 md:grid-cols-3 mb-8 md:gap-8">
+          <div className="custom-card-image mb-6 md:mb-0">
             <div className="rounded-lg shadow-xl card image-full">
               <figure>
-                <img src={avatar_url} alt="user" />
+                <img src={avatar_url} alt="" />
               </figure>
               <div className="card-body justify-end">
                 <h2 className="card-title mb-0">{name}</h2>
-                <p>{login}</p>
+                <p className="flex-grow-0">{login}</p>
               </div>
             </div>
           </div>
+
           <div className="col-span-2">
             <div className="mb-6">
               <h1 className="text-3xl card-title">
@@ -78,6 +83,7 @@ const User = () => {
                 </a>
               </div>
             </div>
+
             <div className="w-full rounded-lg shadow-md bg-base-100 stats">
               {location && (
                 <div className="stat">
@@ -89,7 +95,7 @@ const User = () => {
                 <div className="stat">
                   <div className="stat-title text-md">Website</div>
                   <div className="text-lg stat-value">
-                    <a href={`${blog}`} target="_blank" rel="noreferrer">
+                    <a href={blog} target="_blank" rel="noreferrer">
                       {blog}
                     </a>
                   </div>
@@ -114,45 +120,51 @@ const User = () => {
         </div>
 
         <div className="w-full py-5 mb-6 rounded-lg shadow-md bg-base-100 stats">
-          <div className="stat">
-            <div className="stat-figure text-secondary">
-              <FaUsers className="text-3xl md:text-5xl" />
+          <div className="grid grid-cols-1 md:grid-cols-3">
+            <div className="stat">
+              <div className="stat-figure text-secondary">
+                <FaUsers className="text-3xl md:text-5xl" />
+              </div>
+              <div className="stat-title pr-5">Followers</div>
+              <div className="stat-value pr-5 text-3xl md:text-4xl">
+                {followers}
+              </div>
             </div>
-            <div className="stat-title pr-5">Followers</div>
-            <div className="stat-value pr-5 text-3xl md:text-4xl">
-              {followers}
+
+            <div className="stat">
+              <div className="stat-figure text-secondary">
+                <FaUserFriends className="text-3xl md:text-5xl" />
+              </div>
+              <div className="stat-title pr-5">Following</div>
+              <div className="stat-value pr-5 text-3xl md:text-4xl">
+                {following}
+              </div>
             </div>
-          </div>
-          <div className="stat">
-            <div className="stat-figure text-secondary">
-              <FaUserFriends className="text-3xl md:text-5xl" />
+
+            <div className="stat">
+              <div className="stat-figure text-secondary">
+                <FaCodepen className="text-3xl md:text-5xl" />
+              </div>
+              <div className="stat-title pr-5">Public Repos</div>
+              <div className="stat-value pr-5 text-3xl md:text-4xl">
+                {public_repos}
+              </div>
             </div>
-            <div className="stat-title pr-5">Following</div>
-            <div className="stat-value pr-5 text-3xl md:text-4xl">
-              {following}
-            </div>
-          </div>
-          <div className="stat">
-            <div className="stat-figure text-secondary">
-              <FaCodepen className="text-3xl md:text-5xl" />
-            </div>
-            <div className="stat-title pr-5">Public Repos</div>
-            <div className="stat-value pr-5 text-3xl md:text-4xl">
-              {public_repos}
-            </div>
-          </div>
-          <div className="stat">
-            <div className="stat-figure text-secondary">
-              <FaStore className="text-3xl md:text-5xl" />
-            </div>
-            <div className="stat-title pr-5">Public Gists</div>
-            <div className="stat-value pr-5 text-3xl md:text-4xl">
-              {public_gists}
+
+            <div className="stat">
+              <div className="stat-figure text-secondary">
+                <FaStore className="text-3xl md:text-5xl" />
+              </div>
+              <div className="stat-title pr-5">Public Gists</div>
+              <div className="stat-value pr-5 text-3xl md:text-4xl">
+                {public_gists}
+              </div>
             </div>
           </div>
         </div>
+
+        <RepoList repos={repos} />
       </div>
-      <RepoList repos={repos} />
     </>
   );
 };
